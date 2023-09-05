59b774d5c041c8b3f76cfa4c17f0884c460fa4fb
Lynn Tang <tangminhanh853@gmail.com>
2023-09-05 14:50:41 +0000

    update

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
-def add(x, y):
-    return x + y
-var=
+def toyou(x):
+    return f"hi {x}"
 
-result = add(1, 2)
-print(f"This is the sum: 1, 2, {result}")
+def add(x):
+    return x + 1
+
+def subtract(x):
+    return x - 1
+    
